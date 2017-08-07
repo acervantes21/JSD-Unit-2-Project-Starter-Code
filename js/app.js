fe63315925cd8fd5ab2c7d976e1bd170cde9c5f6
Adri <adriana.cervantes@caa.com>
2017-08-07 15:19:33 -0700

    use different api, append elements

--- js/app.js
+++ js/app.js
@@ -7,67 +7,57 @@ $(function() {
   // DOM is now ready
   var secretKey = config.SDK_KEY;
   var key = config.api_key;
-  var url = "https://accesscontrolalloworiginall.herokuapp.com/https://api.npr.org/query?id=1149";
-  console.log('secretKey', key)
+  // var url = "https://accesscontrolalloworiginall.herokuapp.com/https://api.npr.org/query?id=1149";
+  console.log('secretKey', secretKey)
 
-  // $.get(url + "&numResults=3&requiredAssets=image&date=current&storyCountToday=3&output=JSON&apiKey=" + secretKey, function(response){
-  //   console.log('data', response)
-  //   response.item.forEach(function(result){
-  //   addInfo = result.additionalInfo.id
-  //   console.log('addInfo', result)
-  //   $("h3").html(result.additionalInfo.$text)
-  //   // $("ul").append("<li>"+ result.additionalInfo.$text+"</li>")
+    //call api
+  $.get("https://accesscontrolalloworiginall.herokuapp.com/https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=" + secretKey + "", function(response){
+    console.log('data', response.articles)
+    console.log('this.author', this.author)
     
-  //   })
-  // })
-
-  $.get("https://accesscontrolalloworiginall.herokuapp.com/http://api.eventful.com/json/events/search?keywords=books&location=San+Diego&app_key=" +key + "&id=20218701", function(response){
-    console.log('data', response.venue_name)
-    $("h3").html(response.venue_name)
+    $("ul").append("<li>"+ response.source+"</li>")
     // $('img').attr('src', image);
-    var eventsAvail = response.events.event;
-    if (eventsAvail === 0){
+    var images = response.articles;
+    if (images === 0){
       console.log('no images found');
     } else {
+      console.log('inhere')
       handleResponseSuccess(response);
     }
-    // response.links.link.forEach(function(result){
-    // var link = result.url
-    
-    // console.log('addInfo', link)
-    
+    // response.articles.forEach(function(result){
+    // var author = result.author;
+    // var link = result.url;
+    // console.log('author', author)
+    // $("h3").html(author)
     // $('a').attr('href', link);
-    // $("ul").append("<li>"+ result.additionalInfo.$text+"</li>")
-    
     // })
   })
-    console.log('allData', allData)
-    $.each(allData, function() {
-      console.log('this.url', this.url)
-      console.log('this.url', this.title)
-      $("h3").html(this.title);
-      $('a').attr('href', this.url);
-      var element = $('<img>').attr('src', this.image_url).addClass('image');
-      // $('img').attr('src', this.url); 
-    });
+    
+    // $.each(allData, function() {
+    //   console.log('this.url', this.url)
+    //   console.log('this.url', this.title)
+      
+    //   // $('img').attr('src', this.url); 
+    // });
+  
+  function handleResponseSuccess(response) {
+  var allData = response.articles;
+  console.log('allData', allData)
+  
+  $.each(allData, function() {
+    console.log('this.author', this.author)
+    var articleUrl = $('a').attr('href', this.url);
+    $(".articleContent a").append(articleUrl);
+    // $('img').attr('src', this.urlToImage);
+    $(".articleContent h3").append(this.title);
+    $('h6').html(this.author);
+    // $('h1').html(this.title);
+    // $('p').html(this.description);
+    // $("ul").append("<li>"+allData.description+"</li>")
+    var element = $('<img>').attr('src', this.urlToImage);
+    $(".featuredImage").append(element);
+    // $("ul").append("<li>"+allData.description+"</li>")
+  });
   }
 })
 
-// // debugger
-// $.get("https://accesscontrolalloworiginall.herokuapp.com/https://sugarscape.hearst.io/api/v1/articles?visibility=1&all_images=0&get_image_cuts=0&ignore_cache=0&limit=10&order_by=date+desc", function(results){
-//   console.log(results);
-//   results.data.feed.forEach(function(result){
-//     console.log('result', result)
-//     $("ul").append("<li>"+result.content.title+"</li>")
-//   })
-// })
-
-
-
-function handleResponseSuccess(response) {
-  var allData = response.subcategory;
-  console.log('allData', allData)
-  $.each(allData, function() {
-    $("ul").append("<li>"+innerResult.title.$text+"</li>")
-  });
-}
--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var config = {
-    SDK_KEY: "MDEwNjQyNzU3MDEzNTY3MTI3MzM5ZjUxNg001_",
+    SDK_KEY: "91dfd39c420f4362a4537bbea3f8c186",
     api_key: 'fFMsPQNm4sGHjjmb'
 }
