bca9062277599671475c00f2f2f456ccee912bc5
Adri <adriana.cervantes@caa.com>
2017-08-09 20:03:18 -0700

    Adds sourcelist in searchbox

--- js/app.js
+++ js/app.js
@@ -7,57 +7,49 @@ $(function() {
   // DOM is now ready
   var secretKey = config.SDK_KEY;
   var key = config.api_key;
-  // var url = "https://accesscontrolalloworiginall.herokuapp.com/https://api.npr.org/query?id=1149";
-  console.log('secretKey', secretKey)
 
-    //call api
+  //call api
   $.get("https://accesscontrolalloworiginall.herokuapp.com/https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=" + secretKey + "", function(response){
     console.log('data', response.articles)
     console.log('this.author', this.author)
-    
-    $("ul").append("<li>"+ response.source+"</li>")
+    // var theSource = $('<li>').attr('href', response.source);
+    // $("ul").append(theSource);
+    $('#sourceList').append($('<li>', {}).append($('<a>', { href: '#'}).text(response.source)));
     // $('img').attr('src', image);
-    var images = response.articles;
-    if (images === 0){
-      console.log('no images found');
+    var articles = response.articles;
+    if (articles === 0){
+      console.log('no articles found');
     } else {
       console.log('inhere')
       handleResponseSuccess(response);
     }
-    // response.articles.forEach(function(result){
-    // var author = result.author;
-    // var link = result.url;
-    // console.log('author', author)
-    // $("h3").html(author)
-    // $('a').attr('href', link);
-    // })
+    $(".articleContent h3").on('click',function(e){
+      e.preventDefault();
+      showPopUp($(this));
+    });
   })
     
-    // $.each(allData, function() {
-    //   console.log('this.url', this.url)
-    //   console.log('this.url', this.title)
-      
-    //   // $('img').attr('src', this.url); 
-    // });
-  
+  //success
   function handleResponseSuccess(response) {
   var allData = response.articles;
   console.log('allData', allData)
-  
   $.each(allData, function() {
-    console.log('this.author', this.author)
-    var articleUrl = $('a').attr('href', this.url);
-    $(".articleContent a").append(articleUrl);
-    // $('img').attr('src', this.urlToImage);
-    $(".articleContent h3").append(this.title);
+    var article = $("a").attr('href', this.url);
+    $('img').attr('src', this.urlToImage)
+    $("h3").html(this.title);
     $('h6').html(this.author);
-    // $('h1').html(this.title);
-    // $('p').html(this.description);
     // $("ul").append("<li>"+allData.description+"</li>")
-    var element = $('<img>').attr('src', this.urlToImage);
-    $(".featuredImage").append(element);
-    // $("ul").append("<li>"+allData.description+"</li>")
+    $('.articleContent').append(this.article)
+
   });
+  // $('#main').append(article);
+  }
+
+  function showPopUp(){
+    $('#popUp h1').html(this.title);
+    $('#popUp p').html(this.description)
+    $("a").attr('href', this.url);
+    $('#popUp').removeClass('loader hidden')
   }
 })
 
